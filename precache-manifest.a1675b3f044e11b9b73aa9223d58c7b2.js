9db599b6e10e5a70afedd1c4bd7982c5d3e1a124
JacquelineJia <>
2022-07-26 16:08:02 -0400

    Updates

---
+++ precache-manifest.a1675b3f044e11b9b73aa9223d58c7b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "54ee9f55b80ab939e471",
+    "url": "/static/css/main.a322ac84.chunk.css"
+  },
+  {
+    "revision": "54ee9f55b80ab939e471",
+    "url": "/static/js/main.54ee9f55.chunk.js"
+  },
+  {
+    "revision": "50eb1e2a849d3852b45b",
+    "url": "/static/js/1.50eb1e2a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "00dd0942421c8e04ccf55a24e2f585ec",
+    "url": "/static/media/diana-ding.00dd0942.jpg"
+  },
+  {
+    "revision": "985ec3749e73276deb1e4c2d76eff649",
+    "url": "/static/media/tao-ding.985ec374.jpg"
+  },
+  {
+    "revision": "c25c2126e7ef81b71378610540d00f13",
+    "url": "/static/media/home-page-img.c25c2126.jpg"
+  },
+  {
+    "revision": "b6e976f43ad034f3c7d4a405284cfe04",
+    "url": "/index.html"
+  }
+];
